1d2427f9d52c4376210f290560785dff66737c63
AngshulMajumdar <angshul@iiitd.ac.in>
2026-03-28 19:51:19 +0530

    Create __init__.py

---
+++ autograde/__init__.py
@@ -0,0 +1,14 @@
+"""Generalized multimodal autograding core package."""
+
+__all__ = [
+    "models",
+    "ingestion",
+    "extractors",
+    "rubric",
+    "evaluators",
+    "executor",
+    "integrity",
+    "outputs",
+    "benchmarks",
+]
+
